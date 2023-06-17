9bf263e16100c074aef2d84322d2e7df4deabcb2
raaj-love-to-code <sraajkumar12345@gmail.com>
2023-06-17 15:28:53 +0530

    initiated phaser

--- phaser/js/index.js
+++ phaser/js/index.js
@@ -1,3 +1,28 @@
 $(document).ready(() => {
+    var config = {
+        type: Phaser.AUTO,
+        width: '100%',
+        height: 400,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
     
+    var game = new Phaser.Game(config);
+    
+    function preload ()
+    {
+        this.load.image('sky', '../assets/bg.png');
+    }
+    
+    function create ()
+    {
+        this.add.image(500, 200, 'sky');
+    }
+    
+    function update ()
+    {
+    }
 });
